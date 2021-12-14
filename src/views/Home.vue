7aac58553be8341b5a6092af79dd271630da7030
Luiz Ricardo Ripardo <ricardo_ripardo@hotmail.com>
2021-12-14 11:31:12 -0300

    screen only for mobile container

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,18 @@
 <template>
-  <v-container class="pa-0 fill-height ">
+  <v-container class="fill-height ">
     <v-row class="text-center" no-gutters align="center" justify="center">
       <v-col cols="12">
         <v-avatar size="300">
           <v-img src="@/assets/logo.png" alt="logo"></v-img>
         </v-avatar>
       </v-col>
-      <v-col class="pa-2" cols="12">
-        <router-link to="/presentation">
-          <v-btn class="black--text font-weight-bold" elevation="0" color="primary" tile block x-large>APRESENTAÇÃO
+      <v-col class="py-2" cols="12">
+          <v-btn class="black--text font-weight-bold" elevation="0" color="primary" tile block>
+            APRESENTAÇÃO
           </v-btn>
-        </router-link>
       </v-col>
-      <v-col class="pa-2" cols="12">
-        <v-btn class="black--text font-weight-bold" elevation="0" color="primary" tile block x-large>TECIDOS BÁSICOS
+      <v-col class="py-2" cols="12">
+        <v-btn class="black--text font-weight-bold" elevation="0" color="primary" tile block>TECIDOS BÁSICOS
           DE
           HISTOLOGIA
         </v-btn>
