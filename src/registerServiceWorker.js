b2ec82e525eec1061706b66c5a5dbe5cea50f6f3
Luiz Ricardo Ripardo <ricardo_ripardo@hotmail.com>
2021-12-22 12:18:33 -0300

    auto reload pwa service worker

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -14,6 +14,7 @@ if (process.env.NODE_ENV === 'production') {
             console.log('New content is downloading.')
         }, updated() {
             console.log('New content is available; please refresh.')
+            window.location.reload()
         }, offline() {
             console.log('No internet connection found. App is running in offline mode.')
         }, error(error) {
